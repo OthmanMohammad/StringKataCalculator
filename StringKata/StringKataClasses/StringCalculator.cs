3f9d55c7998e9a2d686ab50ef974df480dc15f66
Mohammad Othman <emranm226@hotmail.com>
2022-12-15 05:32:26 +0000

    finished Steps 1 & 2

---
+++ StringKata/StringKataClasses/StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace StringKata.Classes
+{
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        public int Add(string numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException("Invalid input! Please make sure that the input you are providing is not null.");
+            }
+
+            if (String.IsNullOrWhiteSpace(numbers))
+            {
+                return 0;
+            }
+
+            else
+            {
+                var sum = 0;
+                var numbersArray = numbers.Split(',');
+                foreach (var number in numbersArray)
+                {
+                    sum += int.Parse(number.Trim());
+                }
+                return sum;
+            }
+        }
+    }
+}
---
+++ StringKata/StringKataTests/StringCalculatorShould.cs
@@ -0,0 +1,67 @@
+using System;
+using Xunit;
+using StringKata.Classes;
+
+namespace String_Calculator_Kata.Tests
+{
+    public class StringCalculatorShould
+    {
+        private readonly StringCalculator _stringCalculator;
+
+        public StringCalculatorShould()
+        {
+            this._stringCalculator = new StringCalculator();
+        }
+
+
+        [Fact]
+        public void ShouldThrowArgumentNullExceptionIfInputStringIsNull()
+        {
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => this._stringCalculator.Add(null));
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData("\n", 0)]
+        public void ShouldReturnZeroIfInputStringIsEmptyOrWhiteSpace(string numbers, int expectedSum)
+        {
+            //Act
+            var actual = this._stringCalculator.Add(numbers);
+
+            //Assert
+            Assert.Equal(expectedSum, actual);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("7", 7)]
+        [InlineData("13 ", 13)]
+        [InlineData(" 173", 173)]
+        [InlineData(" 1072183 ", 1072183)]
+        public void ShouldReturnCorrectSumOfOneNumberIfInputStringContainsOneNumberOnly(string numbers, int expectedSum)
+        {
+            //Act
+            var actual = this._stringCalculator.Add(numbers);
+
+            //Assert
+            Assert.Equal(expectedSum, actual);
+        }
+
+        [Theory]
+        [InlineData("1,2,100,105", 208)]
+        [InlineData("7, 9 , 1082,999", 2097)]
+        [InlineData("0, 0,0, 0 ,0", 0)]
+        [InlineData("1000, 2000, 3000, 4000", 10000)]
+        public void ShouldReturnCorrectSumOfAllNumbersIfStringContainsMoreThanOneNumber(string numbers, int expectedSum)
+        {
+            //Act
+            var actual = this._stringCalculator.Add(numbers);
+
+            //Assert
+            Assert.Equal(expectedSum, actual);
+        }
+
+    }
+}
